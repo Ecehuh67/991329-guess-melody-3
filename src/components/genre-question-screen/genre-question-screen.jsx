6a6496fb89714b6260f98094ca38d4dba32453fb
Гендольф <ecehuh67@gmail.com>
2020-02-19 20:39:55 +0300

    added the player

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {GameType} from '../../consts.js';
+import AudioPlayer from "../audio-player/audio-player.jsx";
 
 export default class GenreQuestionScreen extends PureComponent {
   constructor(props) {
@@ -17,25 +18,6 @@ export default class GenreQuestionScreen extends PureComponent {
     const {answers, genre} = question;
 
     return (
-      <section className="game game--genre">
-        <header className="game__header">
-          <a className="game__back" href="#">
-            <span className="visually-hidden">Сыграть ещё раз</span>
-            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
-          </a>
-
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370"
-              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-          </svg>
-
-          <div className="game__mistakes">
-            <div className="wrong"/>
-            <div className="wrong"/>
-            <div className="wrong"/>
-          </div>
-        </header>
-
         <section className="game__screen">
           <h2 className="game__title">Выберите {genre} треки</h2>
           <form
@@ -47,12 +29,10 @@ export default class GenreQuestionScreen extends PureComponent {
           >
             {answers.map((answer, i) => (
               <div key={`${i}-${answer.src}`} className="track">
-                <button className="track__button track__button--play" type="button"/>
-                <div className="track__status">
-                  <audio
-                    src={answer.src}
-                  />
-                </div>
+                <AudioPlayer
+                  isPlaying={i === 0}
+                  src={answer.src}
+                />
                 <div className="game__answer">
                   <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`}
                     id={`answer-${i}`}
@@ -73,7 +53,6 @@ export default class GenreQuestionScreen extends PureComponent {
             <button className="game__submit button" type="submit">Ответить</button>
           </form>
         </section>
-      </section>
     );
   }
 }
